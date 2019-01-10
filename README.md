588d4f9a4b6f09b265ea4e3fe38f3383d4945bc9
0h-n0 <kbu94982@gmail.com>
2019-01-10 13:39:21 +0900

    feat:

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # linensend
+
 send a message from command line to LINE service.
+
+
+## Preperation
+
+1. register your email and password on LINE app in your phone.
+1. create a access token in https://notify-bot.line.me/ja/ .
+
+## Installation
+
+```shell
+$ pip install
+```
+
+## How to use
+
+```shell
+$ linesend -t YOUR_TOKEN -m MESSAGE
+```
+
+
---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+import re
+import setuptools
+from pathlib import Path
+
+p = Path(__file__)
+
+version = "1.0.0"
+
+setup_requires = [
+    'requests',
+]
+install_requires = [
+    ''
+]
+test_require = [
+    '',
+    '',
+    ''
+]
+
+setuptools.setup(
+    name="linesend",
+    version=version,
+    python_requires='>3.6',    
+    author="Koji Ono",
+    author_email="koji.ono@exwzd.com",
+    description=".",
+    url='https://github.com/0h-n0/linesend',
+    long_description=(p.parent / 'README.md').open(encoding='utf-8').read(),
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=test_require,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points = {
+        'console_scripts' : ['linesend = linesend:commandline'],
+    },
+)
